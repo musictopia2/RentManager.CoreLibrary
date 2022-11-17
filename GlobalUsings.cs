95b6d0ad5cd1b272d92696858495eb013d83619b
Andy Walter <musictopia2@hotmail.com>
2022-11-16 18:53:23 -0700

    New extension for registering rent view models.

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -6,3 +6,4 @@ global using Microsoft.AspNetCore.Components;
 global using RentManager.CoreLibrary.ViewModels;
 global using System.Diagnostics.CodeAnalysis;
 global using CommonBasicLibraries.AdvancedGeneralFunctionsAndProcesses.BasicExtensions;
+global using Microsoft.Extensions.DependencyInjection;
---
+++ ServiceExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace RentManager.CoreLibrary;
+public static class ServiceExtensions
+{
+    public static IServiceCollection RegisterRentViewModels(this IServiceCollection services)
+    {
+        services.AddTransient<IViewLeaseViewModel, ViewLeaseViewModel>();
+        services.AddTransient<IEnterLeaseViewModel, EnterLeaseViewModel>();
+        return services;
+    }
+}
